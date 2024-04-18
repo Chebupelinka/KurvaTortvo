c4ba23da1ace02348e5de3fb2529cf069a1ac986
Chebupelinka <spb@dyanovsky.ru>
2024-04-18 15:56:24 +0300

    Redo functions

--- DriveTablets.py
+++ DriveTablets.py
@@ -8,11 +8,15 @@ class Start:
     auth = GoogleAuth()
     auth.LocalWebserverAuth()
     drive = GoogleDrive(auth)
+    all_folders = {}
 
-    def create_and_upload_file(self, file_name='test.txt'):
+    root = '00000000000000000'
+
+    def create_and_upload_file(self, file_name='test.txt', parent_folder=root):
 
         try:
-            my_file = self.drive.CreateFile({"title": f'{file_name}'})
+            my_file = self.drive.CreateFile({'title': f'{file_name}'})
+            my_file['parents'][0]['id'] = parent_folder
             my_file.SetContentString("Text")
             my_file.Upload()
             return f"File {file_name} was uploaded!"
@@ -20,15 +24,28 @@ class Start:
         except Exception as _ex:
             return "Error!"
 
-    def get_folder_id(self):
+    def get_root_folder_id(self):
         root_folder = self.drive.ListFile({'q': "trashed=false"}).GetList()
         for fold in root_folder:
-            print(fold)
+            if str(fold['parents'][0]['isRoot']) == 'True':
+                return fold['parents'][0]['id']
+        # 1Fe9DzGJ30AFR93hk9kQrPl7Qql5RAQ9Q
+        # Print the folder names and IDs
+
+    def get_list_of_all_folders(self):
+        root_folder = self.drive.ListFile({'q': "trashed=false"}).GetList()
+        for fold in root_folder:
+            if self.all_folders.get(fold['parents'][0]['id'], 0) == 0:
+                self.all_folders[str(fold['parents'][0]['id'])] = []
+            d = {str(fold['id']): str(fold['title'])}
+            self.all_folders[str(fold['parents'][0]['id'])].append(d)
         # 1Fe9DzGJ30AFR93hk9kQrPl7Qql5RAQ9Q
         # Print the folder names and IDs
 
     def __init__(self):
-        print()
+
+        self.root = self.get_root_folder_id()
+        self.get_list_of_all_folders()
         #print(self.create_and_upload_file())
 
     if __name__ == '__main__':
@@ -36,4 +53,4 @@ class Start:
 
 
 p = Start()
-p.get_folder_id()
+print(p.all_folders)
